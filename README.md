ebdbaacc93ceefc4ba53549860f4612d912765f4
liaochengzong <2522994502@qq.com>
2018-11-27 10:56:25 +0800

    目录

---
+++ README.md
@@ -0,0 +1,16 @@
+# mpvue-quickstart
+
+> fork 自 [vuejs-templates/webpack](https://github.com/vuejs-templates/webpack)修改而来。
+
+## 基本用法
+``` bash
+$ npm install -g vue-cli
+$ vue init liaochengzong/tesla-quickstart my-project
+$ cd my-project
+$ npm install
+$ npm run dev
+```
+
+更多详细文档请查阅 [quickstart](http://mpvue.com/mpvue/quickstart/)。
+
+bug 或者交流建议等请反馈到 [mpvue/issues](https://github.com/Meituan-Dianping/mpvue/issues)。
---
+++ meta.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "appid": {
+      "type": "string",
+      "required": false,
+      "message": "wxmp appid",
+      "default": "touristappid"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A Mpvue project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    }
+  }
+};
