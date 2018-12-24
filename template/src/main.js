d4ceb0e644042eb77e27d9dc9a289962009f33f4
liaochengzong <2522994502@qq.com>
2018-12-24 13:44:34 +0800

    style中的px适配为rpx

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from '@/App.vue'
+import miniprogramUtil from './mixins/miniprogramUtil'
 /* app-only-begin */
 Vue.config.productionTip = false
 App.mpType = 'app'
 /* app-only-end */
+Vue.mixin(miniprogramUtil)
 const app = new Vue(App)
 app.$mount()
---
+++ template/src/mixins/weexUtil.js
@@ -0,0 +1,8 @@
+export default {
+    methods: {
+        getSizeUnit(){
+            return 'px'
+
+        }
+    }
+}
